77a87ae9ac9f3a3fc392a6659cee583e9074ce99
Sandipan Mondal <office.smondal@gmail.com>
2022-11-17 15:10:10 -0500

    inheritence

--- index.ts
+++ index.ts
@@ -3,27 +3,46 @@
 
 class Person {
     //A instace variable, if it is not declared, is automatically public 
-    name: string = ''
-    age: number = 0
-
-    function greeting(name: string): string{
-        this.name = name;
-        return this.name;
+    name: string
+    age: number
+    constructor(){
+      this.name = "Sandy"
+      this.age = 20
+      console.log("went here")
+    }
+    greeting(name:string): string {
+      this.name = name;
+      return this.name;
     }
  }
  
  // motor vehicle object
- class Motor {
+ class Motor extends Person{
    make: string = ''
    model: string = ''
    color: string = ''
+  
+   constructor(make: string, mode: string, color: string){
+    super();
+    this.make = make
+    this.model = mode
+    this.color = color
+   }
+
+   allInfo = () => {
+    let check = new Person()
+    console.log(check);
+    return `${this.name} is ${this.age} years old and has a ${this.color} ${this.make} ${this.model}`;
+   }
+
  }
  
  // create a new instance of each
  const person1 = new Person()
- const motor1 = new Motor()
+ const motor1 = new Motor("BMW", "3-Series", "Blueish Green")
  
  // warning Property 'age' does not exist on type 'Motor'
-let newName = person1.greeting('John Doe');
+let newName: string = person1.greeting('John Doe');
 
-console.log(newName)
+console.log(newName, person1.age)
+console.log(motor1.allInfo())
